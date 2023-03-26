69a64426c84a1ab3cc923b67d1017a744654ca50
Erna <ernatotu@gmail.com>
2023-03-26 22:06:13 +0700

    add-homework1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            var name = Console.ReadLine();
+            Console.WriteLine(name + ", Привет!");
+        }
+    }
+}
