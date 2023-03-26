ad18f10942bc5831c7c63a2981048b173ee4ba26
Erna <ernatotu@gmail.com>
2023-03-26 22:11:25 +0700

    add HomeWork2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray;
+
+            while (true)
+            {
+                Console.Write("Введите размер массива: ");
+                try
+                {
+                    sizeArray = int.Parse(Console.ReadLine());
+                    if (sizeArray < 2)
+                    {
+                        Console.WriteLine("Массив должен состоять минимум из двух элементов");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Размер массива не может быть отричательным");
+                }
+
+            }
+
+            int[] array = new int[sizeArray];
+            int minInt = -2147483648;
+            int firstMaxVal = minInt;
+            int secondMaxVal = minInt;
+
+            while (true)
+            {
+                try
+                {
+                    for (int i = 0; i < sizeArray; i++)
+                    {
+                        Console.Write($"Введите элемент массива {i}: ");
+                        array[i] = int.Parse(Console.ReadLine());
+                        if (array[i] > firstMaxVal)
+                        {
+                            secondMaxVal = firstMaxVal;
+                            firstMaxVal = array[i];
+                        }
+                        else if (array[i] > secondMaxVal)
+                        {
+                            secondMaxVal = array[i];
+                        }
+                    }
+                    Console.WriteLine($"Второй наибольший элемент массива равен: {secondMaxVal}");
+                    return;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Числа в массиве должны быть целыми");
+                }
+            }
+        }
+    }
+}
